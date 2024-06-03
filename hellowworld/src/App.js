2cdca47f236d37805cfad1c209794178e0df8dc9
Marano <77554696+MaranoMMV@users.noreply.github.com>
2024-06-03 14:14:02 -0300

    First Commit

---
+++ hellowworld/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Missão dada é missão cumprida!</h1>
+      <h2>  Tarefa 1 - Concluida </h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui ut quibusdam animi in? Blanditiis, debitis sint. Quia vitae nostrum similique dolorem est quibusdam quam amet, voluptatem fuga autem corrupti quod.</p>
+    a
+    </div>
+    
+  )
+}
+
+export default App;
